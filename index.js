a5966f051bf559f5854ff802dd4650aab08ec202
norisuke-nu <61041875+norisuke-nu@users.noreply.github.com>
2020-07-25 13:49:02 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ $(function(){
 return false;
   });
 });
+
+$(function(){
+$(".btn-gnavi").on("click", function(){
+    // ハンバーガーメニューの位置を設定
+    var rightVal = 0;
+    if($(this).hasClass("open")) {
+        // 位置を移動させメニューを開いた状態にする
+        rightVal = -300;
+        // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+        $(this).removeClass("open");
+    } else {
+        // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+        $(this).addClass("open");
+    }
+
+    $("#global-navi").stop().animate({
+        right: rightVal
+    }, 200);
+});
+});
+
+$(function(){
+  $('#global-navi a[href]').on('click', function(event) {
+$('.btn-gnavi').trigger('click');
+});
+
+});
